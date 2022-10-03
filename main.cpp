fcb9a18d3791c17bb59a729cf2e68a06b19bb43a
chenxingtong <1244017825@qq.com>
2022-10-03 17:12:44 +0000

    test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 /*
  * @Date: 2022-10-03 16:50:37
- * @LastEditors: error: git config user.name && git config user.email & please
+ * @LastEditors: chenxingtong 1244017825@qq.com
  * set dead value or install git
- * @LastEditTime: 2022-10-03 16:51:15
+ * @LastEditTime: 2022-10-03 17:12:40
  * @FilePath: /VortexServer/main.cpp
  */
 #include <iostream>
+using namespace std;
+int main() { cout << "dada" << endl; }
