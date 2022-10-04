256bf1c5ccb20765915d309f4bff43082aa090c3
chenxingtong <1244017825@qq.com>
2022-10-04 09:17:18 +0000

    提交main

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
  * @Date: 2022-10-03 16:50:37
  * @LastEditors: chenxingtong 1244017825@qq.com
  * set dead value or install git
- * @LastEditTime: 2022-10-03 17:12:40
+ * @LastEditTime: 2022-10-04 07:01:56
  * @FilePath: /VortexServer/main.cpp
  */
 #include <iostream>
 using namespace std;
-int main() { cout << "dada" << endl; }
+int main() {
+  cout << "dada" << endl;
+  cout << "test" << endl;
+  int a = 10;
+  int b = 10;
+  int c = a + b;
+  cout << "c==" << c << endl;
+}
