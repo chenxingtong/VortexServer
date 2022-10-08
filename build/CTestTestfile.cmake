20e96728252bf64a63666404a862ea0a0a6df9eb
chenxingtong <1244017825@qq.com>
2022-10-08 15:22:02 +0000

    删除build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jason/VortexServer
-# Build directory: /home/jason/VortexServer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
