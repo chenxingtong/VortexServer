35ea595ecc9d4e88e53efffa1bb20edf09a84c4c
chenxingtong <1244017825@qq.com>
2022-11-23 15:15:11 +0000

    main函数优化

--- main.cpp
+++ main.cpp
@@ -2,16 +2,9 @@
  * @Date: 2022-10-03 16:50:37
  * @LastEditors: chenxingtong 1244017825@qq.com
  * set dead value or install git
- * @LastEditTime: 2022-10-08 15:32:40
+ * @LastEditTime: 2022-11-23 15:15:06
  * @FilePath: /VortexServer/main.cpp
  */
 #include <iostream>
 using namespace std;
-int main() {
-  cout << "dada" << endl;
-  cout << "test" << endl;
-  int a = 10;
-  int b = 20;
-  int c = a + b;
-  cout << "c==" << c << endl;
-}
+int main() {}
